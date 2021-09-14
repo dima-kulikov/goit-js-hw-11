d7ee971119c2cde906b051100ba519165213b6f0
dima-kulikov <skaizy44@gmail.com>
2021-09-14 18:17:21 +0300

    14/09
    урок работа с тайм аутами

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,1 +1,14 @@
-console.log('work');
+const myCode = (time)=>
+    console.log(
+    `Время идет ${time}ms ${Date.now()}`);
+
+    let counter = 0;
+
+const interval = setInterval(()=>{
+    console.log('Подписка на рассылку');
+    counter += 1;
+    console.log(counter);
+},1000);
+
+
+
