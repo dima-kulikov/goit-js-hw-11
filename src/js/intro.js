87401a8d6eab7924755e991a722cd8f2941ac287
dima-kulikov <skaizy44@gmail.com>
2021-09-15 18:20:19 +0300

    15.09.
    15.09.

--- src/js/intro.js
+++ src/js/intro.js
@@ -3,11 +3,18 @@ const myCode = (time)=>
     `Время идет ${time}ms ${Date.now()}`);
 
     let counter = 0;
+    let pp = 0
 
 const interval = setInterval(()=>{
-    console.log('Подписка на рассылку');
+    if(counter <10){
+        console.log('Подписка на рассылку');
     counter += 1;
     console.log(counter);
+    } else{
+        clearInterval(interval);
+        console.log('END')
+    }
+    console.log(`ведем счет  ${pp+=1}`)
 },1000);
 
 
