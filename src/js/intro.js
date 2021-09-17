31097c3e60e2e5f81dfb5142dd0652286f07361f
dima-kulikov <skaizy44@gmail.com>
2021-09-17 15:09:50 +0300

    Update intro.js
    в

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,21 +1,37 @@
-const myCode = (time)=>
-    console.log(
-    `Время идет ${time}ms ${Date.now()}`);
+// const date = new Date();
+// console.log(date.getTime());
 
-    let counter = 0;
-    let pp = 0
+const timer = {
+    start(){
+        this.startTime = Date.now();
+        
+        this.timerId = setInterval(()=>{
+console.log('worck!');
+        },1000);
+    },
+};
 
-const interval = setInterval(()=>{
-    if(counter <10){
-        console.log('Подписка на рассылку');
-    counter += 1;
-    console.log(counter);
-    } else{
-        clearInterval(interval);
-        console.log('END')
-    }
-    console.log(`ведем счет  ${pp+=1}`)
-},1000);
+timer.start();
+
+
+// const myCode = (time)=>
+//     console.log(
+//     `Время идет ${time}ms ${Date.now()}`);
+
+//     let counter = 0;
+//     let pp = 0
+
+// const interval = setInterval(()=>{
+//     if(counter <10){
+//         console.log('Подписка на рассылку');
+//     counter += 1;
+//     console.log(counter);
+//     } else{
+//         clearInterval(interval);
+//         console.log('END')
+//     }
+//     console.log(`ведем счет  ${pp+=1}`)
+// },1000);
 
 
 
